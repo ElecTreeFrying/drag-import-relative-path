5e03392209fd1cd972ed29b99a9e2454f17d9d30
John James G. Ermitaño <jjgermitano@gmail.com>
2022-08-21 05:57:03 +0800

    unit testing poc

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,13 +3,16 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
+suite('Extension activation testing', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+	test('Check if 1 subscription is registered', async () => {
+
+    const extension = vscode.extensions.getExtension("ElecTreeFrying.drag-import-relative-path");
+    const extensionContext = await extension.activate();
+
+    console.log('@@@ ', extensionContext);
+
+  });
 });
