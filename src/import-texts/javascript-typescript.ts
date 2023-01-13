49ef0595d198ee4f5c9c56a82950b82062dc6f52
John James G. Ermitaño <jjgermitano@gmail.com>
2023-01-13 19:08:34 +0800

    angular compatibility

--- src/import-texts/javascript-typescript.ts
+++ src/import-texts/javascript-typescript.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 import { importStyle } from '../providers';
 import { ImportStyle } from '../model';
@@ -40,11 +41,25 @@ export function getTypescriptImport(relativePath: string): vscode.SnippetString 
 
   switch (configValue as number) {
     case 0:  return new vscode.SnippetString(`import $1 from '${relativePath}';`);
-    case 1:  return new vscode.SnippetString(`import { $1 } from '${relativePath}';`);
+    case 1:  return new vscode.SnippetString(`import { ${importName(relativePath)} } from '${relativePath}';`);
     case 2:  return new vscode.SnippetString(`import { $1 as $2 } from '${relativePath}';`);
     case 3:  return new vscode.SnippetString(`import * as $1 from '${relativePath}';`);
     case 4:  return new vscode.SnippetString(`import '${relativePath}';`);
     default: return new vscode.SnippetString(`import { $1 } from '${relativePath}';`);
   }
 
+}
+
+/**
+ * Returns a customized import name if the file is an Angular component.
+ * @param {string} relativePath Relative path of dragged file and active text editor.
+ * @returns Import name
+ */
+function importName(relativePath: string): string {
+  if (relativePath.includes('.component')) {
+    const snackCase = path.basename(relativePath).replace(/\./g, '-');
+    return snackCase.split('-').map(e => e[0].toUpperCase() + e.slice(1)).join('');  
+  } else {
+    return '$1';
+  }
 }
